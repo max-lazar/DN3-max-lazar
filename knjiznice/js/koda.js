4adca2c7c253efae10d72594ac59ef73ec8206af
max-lazar <lazar.max5@gmail.com>
2022-05-07 13:29:10 +0200

    initialcommit

---
+++ knjiznice/js/koda.js
@@ -0,0 +1,24 @@
+const TEACHING_API_BAZA = "TO SPREMENITE NA VAŠE GITHUB UPORABNIŠKO IME";
+const SENSEI_RACUN = "TO SPREMENITE NA VAŠ JAVNI KLJUČ (NASLOV DENARNICE)";
+
+let SENSEI_BC_RPC_URL = "https://sensei.lavbic.net:8546";
+let TEACHING_API_BASE_URL =
+  "https://teaching.lavbic.net/api/OIS/baza/" + TEACHING_API_BAZA + "/podatki/";
+
+/**
+ * Generator podatkov za novega uporabnika, ki bo uporabljal aplikacijo. Pri
+ * generiranju podatkov je potrebno najprej kreirati nov scenarij s specifičnimi
+ * podatki, ki se nanašajo na scenarij.
+ *
+ * @param stScenarija zaporedna številka scenarija (1, 2 ali 3)
+ * @return scenarijId generiranega scenarija
+ */
+function generirajScenarij(stScenarija) {
+  scenarijId = "";
+
+  // TODO: Potrebno implementirati
+
+  return scenarijId;
+}
+
+// TODO: Tukaj implementirate funkcionalnosti, ki jo podpira vaša aplikacija
